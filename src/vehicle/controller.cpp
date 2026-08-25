29b529ccb67eb331a89fb3eee743fb59c5fbba04
Tinbell <diknit0118@gmail.com>
2026-08-25 10:12:47 +0900

    コントローラのDゲインのかかり具合を調整

--- src/vehicle/controller.cpp
+++ src/vehicle/controller.cpp
@@ -3,6 +3,9 @@
 float pid_integral=0.0F;
 float pid_previous_error=0.0F;
 
+float alpha = 0.5F; // 平滑化係数（0 < alpha < 1）
+uint8_t previous_pwm = 0; // 前回のPWM値
+
 // 課題としてコントローラを設計させる
 
 //--------------------------------------------------
@@ -20,7 +23,7 @@ uint8_t calculate_pid_pwm(int32_t current_speed_cm_s, int32_t target_speed_cm_s)
     float I = pid_ki * pid_integral;
 
     // D制御
-    float derivative = (error - pid_previous_error) * static_cast<float>(CONTROL_INTERVAL_MS) / 1000 ;
+    float derivative = (error - pid_previous_error);
     float D = pid_kd * derivative;
     pid_previous_error = error;
 
