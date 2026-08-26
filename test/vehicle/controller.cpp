541f891e93d8d60ab417e19cdb0b1399dc6151f8
Tinbell <diknit0118@gmail.com>
2026-08-26 14:47:43 +0900

    コントローラの変数の一部の名前を変更

--- test/vehicle/controller.cpp
+++ test/vehicle/controller.cpp
@@ -1,30 +1,41 @@
 #include "config.hpp"
 
-float integral=0;
-float previousError=0;
+float pid_integral=0.0F;
+float pid_previous_error=0.0F;
 
+uint8_t previous_pwm = 0; // 前回のPWM値
+
+// 課題としてコントローラを設計させる
 
 //--------------------------------------------------
 // コントローラ関数
 //--------------------------------------------------
-uint8_t PIDcontroller(int currentSpeed, int targetSpeed){
-    float pwm ;
-    float dt = INTERVAL_MS/1000;
+uint8_t calculate_pid_pwm(int32_t current_speed_cm_s, int32_t target_speed_cm_s){
 
-        // TODO
+    // P制御
+    const float error = static_cast<float>(target_speed_cm_s - current_speed_cm_s);
+    float P = pid_kp * error;
 
-    return (uint8_t)constrain(pwm,MIN_PWM,MAX_PWM);
+    // I制御
+    pid_integral += error * static_cast<float>(CONTROL_INTERVAL_MS) / 1000 ; 
+    pid_integral = constrain(pid_integral,-PID_MAX_INTEGRAL,PID_MAX_INTEGRAL);
+    float I = pid_ki * pid_integral;
+
+    // D制御
+    float derivative = (error - pid_previous_error);
+    float D = pid_kd * derivative;
+    pid_previous_error = error;
+
+    float pwm = P + I + D;
+    pwm = constrain(pwm,MIN_PWM,MAX_PWM);
+
+    return static_cast<uint8_t>(pwm);
 }
 
 //--------------------------------------------------
 // コントローラ初期化関数
 //--------------------------------------------------
-void ClearController(){
-    integral=0;
-    previousError=0;
-    targetSpeed = FirstTargetSpeed;
+void reset_controller(){
+    pid_integral=0.0F;
+    pid_previous_error=0.0F;
 }
-
-// PIDcontrollerを設計せよ
-// currentSpeed[cm/s],targetSpeed[cm/s],積分値は±MAX_INTEGRALの範囲を超えてはならない
-// 各ゲインはKP，KI，KDで定義される
