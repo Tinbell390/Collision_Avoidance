7b5c41b27e548a20100513f8aeb2cbbf157560b2
Tinbell <diknit0118@gmail.com>
2026-08-26 15:09:23 +0900

    学生用に一部穴埋めにしたコントローラを作成

--- src/vehicle/controller.cpp
+++ src/vehicle/controller.cpp
@@ -1,7 +1,7 @@
 #include "config.hpp"
 
-float pid_integral=0.0F;
-float pid_previous_error=0.0F;
+float pid_integral=0.0F;                    //累積偏差
+float pid_previous_error=0.0F;              //前回の偏差
 
 uint8_t previous_pwm = 0; // 前回のPWM値
 
@@ -17,12 +17,12 @@ uint8_t calculate_pid_pwm(int32_t current_speed_cm_s, int32_t target_speed_cm_s)
     float P = pid_kp * error;
 
     // I制御
-    pid_integral += error * static_cast<float>(CONTROL_INTERVAL_MS) / 1000 ; 
+    pid_integral += error * (static_cast<float>(CONTROL_INTERVAL_MS) / 1000) ; 
     pid_integral = constrain(pid_integral,-PID_MAX_INTEGRAL,PID_MAX_INTEGRAL);
     float I = pid_ki * pid_integral;
 
     // D制御
-    float derivative = (error - pid_previous_error);
+    float derivative = (error - pid_previous_error) / (static_cast<float>(CONTROL_INTERVAL_MS) / 1000);
     float D = pid_kd * derivative;
     pid_previous_error = error;
 
--- test/vehicle/controller.cpp
+++ test/vehicle/controller.cpp
@@ -1,9 +1,7 @@
 #include "config.hpp"
 
-float pid_integral=0.0F;
-float pid_previous_error=0.0F;
-
-uint8_t previous_pwm = 0; // 前回のPWM値
+float pid_integral=0.0F;            //累積偏差
+float pid_previous_error=0.0F;      //前回の偏差
 
 // 課題としてコントローラを設計させる
 
@@ -13,23 +11,17 @@ uint8_t previous_pwm = 0; // 前回のPWM値
 uint8_t calculate_pid_pwm(int32_t current_speed_cm_s, int32_t target_speed_cm_s){
 
     // P制御
-    const float error = static_cast<float>(target_speed_cm_s - current_speed_cm_s);
-    float P = pid_kp * error;
+
 
     // I制御
-    pid_integral += error * static_cast<float>(CONTROL_INTERVAL_MS) / 1000 ; 
-    pid_integral = constrain(pid_integral,-PID_MAX_INTEGRAL,PID_MAX_INTEGRAL);
-    float I = pid_ki * pid_integral;
+
 
     // D制御
-    float derivative = (error - pid_previous_error);
-    float D = pid_kd * derivative;
-    pid_previous_error = error;
 
-    float pwm = P + I + D;
-    pwm = constrain(pwm,MIN_PWM,MAX_PWM);
 
-    return static_cast<uint8_t>(pwm);
+    // PWM出力（MIN_PWM～MAX_PWMの範囲に制限）
+
+
 }
 
 //--------------------------------------------------
@@ -39,3 +31,18 @@ void reset_controller(){
     pid_integral=0.0F;
     pid_previous_error=0.0F;
 }
+
+//--------------------------------------------------
+// PIDcontrollerを設計せよ
+//
+// 各ゲインはKP，KI，KDで定義される。
+// 1フレームの時間はCONTROL_INTERVAL_MS[ms]で定義されている。
+// 
+// current_speed_cm_s, target_speed_cm_sは[cm/s]であることに注意すること。
+//
+// MIN_PWM～MAX_PWMの範囲に制限して出力すること。
+//
+// 累積偏差（積分値）が±MAX_INTEGRALを超えないよう制限すること。
+//
+// 比例項・積分項・微分項を用いてPID制御を実装せよ。
+//--------------------------------------------------
