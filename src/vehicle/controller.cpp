ee015ce28e4f020516d53a560468e731d5f84e19
Tinbell <diknit0118@gmail.com>
2026-08-27 16:05:09 +0900

    コントローラを簡略化

--- src/vehicle/controller.cpp
+++ src/vehicle/controller.cpp
@@ -17,12 +17,12 @@ uint8_t calculate_pid_pwm(int32_t current_speed_cm_s, int32_t target_speed_cm_s)
     float P = pid_kp * error;
 
     // I制御
-    pid_integral += error * (static_cast<float>(CONTROL_INTERVAL_MS) / 1000) ; 
+    pid_integral += error; 
     pid_integral = constrain(pid_integral,-PID_MAX_INTEGRAL,PID_MAX_INTEGRAL);
     float I = pid_ki * pid_integral;
 
     // D制御
-    float derivative = (error - pid_previous_error) / (static_cast<float>(CONTROL_INTERVAL_MS) / 1000);
+    float derivative = (error - pid_previous_error);
     float D = pid_kd * derivative;
     pid_previous_error = error;
 
