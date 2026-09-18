87cbc46284fba465be5c3fb511e33bb5304debaa
Tinbell <diknit0118@gmail.com>
2026-09-18 16:12:19 +0900

    走行距離と初期速度を調整

--- include/config.hpp
+++ include/config.hpp
@@ -13,14 +13,14 @@ constexpr int32_t VEHICLE_LENGTH_MM              = 160;   // 車両の長さ（�
 constexpr uint8_t VEHICLE_COUNT                  = 2;     // ビークルの台数
 
 // 交差点関連の距離
-constexpr int32_t DIST_TO_INTERSECTION_ENTRY_MM  = 2500;  // 交差点進入位置までの距離（ミリメートル）
+constexpr int32_t DIST_TO_INTERSECTION_ENTRY_MM  = 2000;  // 交差点進入位置までの距離（ミリメートル）
 constexpr int32_t DIST_TO_INTERSECTION_EXIT_MM   = 200;   // 交差点退出位置までの距離（ミリメートル）
 
 // ============================================================
 // 速度パラメータ (Speed)
 // ============================================================
-constexpr int32_t DEFAULT_SPEED_CM_S             = 100;   // デフォルト速度（センチメートル/秒）
-constexpr int32_t MAX_SPEED_CM_S                 = 200;   // 最大速度（センチメートル/秒）
+constexpr int32_t DEFAULT_SPEED_CM_S             = 200;   // デフォルト速度（センチメートル/秒）
+constexpr int32_t MAX_SPEED_CM_S                 = 300;   // 最大速度（センチメートル/秒）
 constexpr int32_t MIN_SPEED_CM_S                 = 20;    // 最小速度（センチメートル/秒）
 constexpr uint8_t SPEED_THRESHOLD_CM_S           = 5;     // 到着時間を計算しても良い速度閾値（センチメートル/秒）
 
